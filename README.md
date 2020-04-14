baa18f38cb786e1ab109bcf0b3448e0ea99eecf3
Dan Buck <35202075+danbuck84@users.noreply.github.com>
2020-04-14 17:10:59 -0300

    new form update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Minha sobrinha havia me pedido para votar várias vezes em uns nomes
-nesse formulário do Google. Vi, aí, uma boa forma de estudar um pouco
-de automação com Python, e escrevi esse robozinho que vota em três
-nomes por um determinado número de vezes.
+num Google Form. Vi aqui uma boa forma de estudar um pouco de
+automação com Python, e escrevi esse robozinho que vota em ~três nomes~
+um ou mais nomes por um determinado número de vezes.
 
 Bem simples, bem tranquilinho.
 
--- auto_form_filling.py
+++ auto_form_filling.py
@@ -14,66 +14,87 @@ driver.get('https://docs.google.com/forms/d/e/1FAIpQLSdTWwfdQUKmxP8XYFy8AEEepEIu
 wait = ui.WebDriverWait(driver, 10)
 wait.until(page_is_loaded)
 
-for x in range(0,99): # selecione o número de vezes que o script vai rodar (neste caso, 100 vezes)
+for x in range(0,499): # selecione o número de vezes que o script vai rodar (neste caso, 100 vezes)
     opt_body = driver.find_elements_by_tag_name('Span')
     for option in opt_body:
-        if option.text == 'Opção 6': # selecione uma opção (para este caso, a opção 6 que mostra uma faixa de pedestre)
+        if option.text == 'Opção 4': # selecione uma opção (para este caso, a opção 4 que mostra uma vaca animada)
             option.click()
             break
 
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div/span/span').click()
 
-    opt_tang = driver.find_elements_by_tag_name('Span')
-    for opt in opt_tang:
-        if opt.text == 'Tang': # selecione um nome
+    opt_big = driver.find_elements_by_tag_name('Span')
+    for opt in opt_big:
+        if opt.text == 'Big': # selecione um nome
             opt.click()
             break
 
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span/span').click()
 
-    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("something@gmail.com") # coloque aqui o email para validar a votação
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("daniel.buck@gmail.com") # coloque aqui o email para validar a votação
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span').click()
     
     driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a').click()
 
     opt_body = driver.find_elements_by_tag_name('Span')
     for option in opt_body:
-        if option.text == 'Opção 6': # selecione uma opção (para este caso, a opção 6 que mostra uma faixa de pedestre)
+        if option.text == 'Opção 4': # selecione uma opção (para este caso, a opção 4 que mostra uma vaca animada)
             option.click()
             break
 
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div/span/span').click()
 
-    opt_victor = driver.find_elements_by_tag_name('Span')
-    for opt in opt_victor:
-        if opt.text == 'Victor': # selecione um nome
+    opt_big = driver.find_elements_by_tag_name('Span')
+    for opt in opt_big:
+        if opt.text == 'Big': # selecione um nome
             opt.click()
             break
 
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span/span').click()
 
-    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("something@gmail.com") # coloque aqui o email para validar a votação
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("erika.carollpaiva@gmail.com") # coloque aqui o email para validar a votação
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span').click()
     
     driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a').click()
 
     opt_body = driver.find_elements_by_tag_name('Span')
     for option in opt_body:
-        if option.text == 'Opção 6': # selecione uma opção (para este caso, a opção 6 que mostra uma faixa de pedestre)
+        if option.text == 'Opção 4': # selecione uma opção (para este caso, a opção 4 que mostra uma vaca animada)
             option.click()
             break
 
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div/span/span').click()
 
-    opt_ju = driver.find_elements_by_tag_name('Span')
-    for opt in opt_ju:
-        if opt.text == 'Ju Bonello': # selecione um nome
+    opt_big = driver.find_elements_by_tag_name('Span')
+    for opt in opt_big:
+        if opt.text == 'Big': # selecione um nome
             opt.click()
             break
 
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span/span').click()
 
-    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("something@gmail.com") # coloque aqui o email para validar a votação
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("teixeira.mgt1@gmail.com") # coloque aqui o email para validar a votação
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span').click()
+    
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a').click()
+
+    opt_body = driver.find_elements_by_tag_name('Span')
+    for option in opt_body:
+        if option.text == 'Opção 4': # selecione uma opção (para este caso, a opção 4 que mostra uma vaca animada)
+            option.click()
+            break
+
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div/span/span').click()
+
+    opt_big = driver.find_elements_by_tag_name('Span')
+    for opt in opt_big:
+        if opt.text == 'Big': # selecione um nome
+            opt.click()
+            break
+
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span/span').click()
+
+    driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("ipod.buck@gmail.com") # coloque aqui o email para validar a votação
     driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[3]/div[1]/div/div[2]/span').click()
     
     driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a').click()
